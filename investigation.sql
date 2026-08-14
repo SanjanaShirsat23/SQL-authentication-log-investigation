fc8a85a0d1589123d0eab177d2460e29334b8024
SanjanaShirsat23 <sanjanashirsat23@gmail.com>
2026-08-14 12:01:01 +0530

    Create investigation.sql

---
+++ investigation.sql
@@ -0,0 +1,9 @@
+-- 1. Count failed login attempts
+
+-- 2. Identify users with repeated failures
+
+-- 3. Identify suspicious IP addresses
+
+-- 4. Investigate successful logins after failures
+
+-- 5. Final investigation query
